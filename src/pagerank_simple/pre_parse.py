914cb9104240172b31561b95a7a232286ed24974
Tom Taylor <tom@tommyt.co.uk>
2016-01-02 16:00:10 +0000

    Preparser to calculate initial PR and formatted file for mapred

--- src/pagerank_simple/pre_parse.py
+++ src/pagerank_simple/pre_parse.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python2
-from sys import stdin, stdout, stderr, exit
+from sys import stdin, stdout
 import re
 import json
 
 
 def pre_parse():
+    """
+    Reads original file on stdin, calculates initial PR as 1/n and outputs adjeceny list with each inbound PR stored
+    for every node in the format:
+
+    dest    [[N1, PR1], [N2, PR2], ... [Nn, PRn]]
+    (Value is JSON serialised)
+
+    Although not space/bandwidth efficient, it allows each node to be processed independently.  Using Pickle(2) would be
+    much more efficient but not human readable for debugging.  Pickle is used however between mapper and reducer.
+    """
     adj_list = dict()
     # RE to match useful lines
     is_a_row = re.compile('(\d+)\t(\d+)')
@@ -14,8 +24,8 @@ def pre_parse():
         # Populate adjecency list
         m = is_a_row.match(line)
         if m:
-            src = m.group(1)
-            dst = m.group(2)
+            src = int(m.group(1))
+            dst = int(m.group(2))
             if dst in adj_list:
                 adj_list[dst].append([src, 1.0])
             else:
